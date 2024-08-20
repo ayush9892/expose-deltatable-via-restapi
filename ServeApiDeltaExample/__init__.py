46a5f3db8fd49ac6cef0ec6b5a17a7983b826180
unknown <ashutoshjha9892@gmail.com>
2024-08-20 16:13:17 +0530

    Update __init__.py

--- ServeApiDeltaExample/__init__.py
+++ ServeApiDeltaExample/__init__.py
@@ -12,50 +12,23 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     start_time = time.perf_counter()
     # Don't store credentials in code in PRD. Instead, use Managed Identity to authenticate to storage.
-    TENANT_ID = "<<your tenant id>>"
+    TENANT_ID = "84c31ca0-ac3b-4eae-ad11-519d80233e6f"
     # Your Service Principal App ID
-    CLIENT_ID = "<<your spn app id>>" # c1dfact-d-spn
+    CLIENT_ID = "790e7097-4f36-4969-b128-9e3933fc7fb7" # c1dfact-d-spn
     # Your Service Principal Password
-    CLIENT_SECRET = "<<your spn secret>>"
-    solution_type = req.params.get('solution_type')
+    CLIENT_SECRET = "tpU8Q~Wju3TBk0QMgYIldEp.zwRe.p-gylEkTbx3"
     sql_query = req.params.get('sql_query')
 
-    if solution_type == "direct":
-        storage_options={'tenant_id': TENANT_ID, 'client_id': CLIENT_ID, 'client_secret': CLIENT_SECRET }
-        dt = DeltaTable("abfss://<<container name>>@<<storage account name>>.dfs.core.windows.net/<<delta folder name>>", storage_options=storage_options)
-        parquet_read_options = {
-            'coerce_int96_timestamp_unit': 'ms',  # Coerce int96 timestamps to a particular unit
-        }
-        pyarrow_dataset = dt.to_pyarrow_dataset(parquet_read_options=parquet_read_options)
-        silver_fact_sale = duckdb.arrow(pyarrow_dataset)
-        results = duckdb.query(sql_query).fetchall()
+    storage_options={'tenant_id': TENANT_ID, 'client_id': CLIENT_ID, 'client_secret': CLIENT_SECRET }
+    dt = DeltaTable("abfss://presentation@synapselearningadls.dfs.core.windows.net/<<delta folder name>>", storage_options=storage_options)
+    parquet_read_options = {
+        'coerce_int96_timestamp_unit': 'ms',  # Coerce int96 timestamps to a particular unit
+    }
+    pyarrow_dataset = dt.to_pyarrow_dataset(parquet_read_options=parquet_read_options)
+    silver_fact_sale = duckdb.arrow(pyarrow_dataset)
+    results = duckdb.query(sql_query).fetchall()
 
-        end_time = time.perf_counter()
-        execution_time = end_time - start_time
+    end_time = time.perf_counter()
+    execution_time = end_time - start_time
 
-        return func.HttpResponse(str(execution_time) + "/n" + f"{results}")
-    else:
-        if solution_type == "synapse_serverless":
-            server="<<synapse serverless name>>-ondemand.sql.azuresynapse.net"
-            database="<<database name>>"
-        elif solution_type == "data_copy_sql":
-            server="<<SQL server name>>.database.windows.net"
-            database="<database name>>"
-        else:
-            return func.HttpResponse("No solution selected")
-            
-        credential = ClientSecretCredential(TENANT_ID, CLIENT_ID, CLIENT_SECRET)
-        token_bytes = credential.get_token("https://database.windows.net/.default").token.encode("UTF-16-LE")
-        token_struct = struct.pack(f'<I{len(token_bytes)}s', len(token_bytes), token_bytes)
-        SQL_COPT_SS_ACCESS_TOKEN = 1256  # This connection option is defined by microsoft in msodbcsql.h
-        connstr = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database
-        conn = pyodbc.connect(connstr, attrs_before={SQL_COPT_SS_ACCESS_TOKEN: token_struct})
-
-        cursor = conn.cursor()
-        cursor.execute(sql_query)
-        row = cursor.fetchall()
-
-        end_time = time.perf_counter()
-        execution_time = end_time - start_time
-
-        return func.HttpResponse(str(execution_time) + "/n" + str(row))
+    return func.HttpResponse(str(execution_time) + "/n" + f"{results}")
